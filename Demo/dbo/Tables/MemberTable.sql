3b8fef4f441c1f3a98a31c9667048b45e0f54d3a
Suresh Bonda <suresh.bonda@caremore.com>
2020-11-05 16:12:52 -0800

    First commit to source code

---
+++ Demo/dbo/Tables/MemberTable.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[MemberTable] (
+    [MemberID]          NCHAR (10) NOT NULL,
+    [MemberFirstName]   NCHAR (20) NOT NULL,
+    [MemberLastName]    NCHAR (20) NOT NULL,
+    [MemberAddress]     NCHAR (40) NOT NULL,
+    [MemberAddlAddress] NCHAR (10) NOT NULL,
+    [MemberCity]        NCHAR (10) NOT NULL,
+    [MemberState]       NCHAR (10) NOT NULL,
+    [MemberZip]         NCHAR (10) NOT NULL
+);
+
